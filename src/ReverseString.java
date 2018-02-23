78971b9c6a0a14560d0fc7b3cd0a208ec4698f53
FreddieSun <freddiesun95@gmail.com>
2018-02-23 12:03:54 -0500

    2/23

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,6 +1,19 @@
 public class ReverseString {
     public String reverseString(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        return sb.reverse().toString();
+        //StringBuilder sb=new StringBuilder(s);
+        //return sb.reverse().toString();
+        char[] cs=s.toCharArray();
+        int i=0,j=cs.length-1;
+        while(i<j){
+            char temp=cs[j];
+            cs[j]=cs[i];
+            cs[i]=temp;
+            i++;
+            j--;
+        }
+
+        return String.valueOf(cs);
     }
+
+
 }
