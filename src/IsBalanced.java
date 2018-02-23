393cd1477fc5240ca6affa4a249b034661f47572
FreddieSun <freddiesun95@gmail.com>
2018-02-23 15:05:36 -0500

    2/23

--- src/IsBalanced.java
+++ src/IsBalanced.java
@@ -7,5 +7,21 @@ public class IsBalanced {
         TreeNode(int x) { val = x; }
     }
 
+    public boolean isBalanced(TreeNode root) {
+        if(root==null) return true;
+        int left=height(root.left);
+        int right=height(root.right);
+        return Math.abs(left-right)<=1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int height(TreeNode root){
+        if(root == null)
+            return 0;
+
+        int resL = 1+height(root.left);
+        int resR = 1+height(root.right);
+        return resL>resR ? resL:resR;
+
+    }
 
 }
