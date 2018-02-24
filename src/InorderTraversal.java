7fa8ccad258c41ad4b30a0ca0477c64857df3c06
FreddieSun <freddiesun95@gmail.com>
2018-02-24 13:26:54 -0500

    2/24

--- src/InorderTraversal.java
+++ src/InorderTraversal.java
@@ -4,32 +4,33 @@ import java.util.Stack;
 
 public class InorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> res=new ArrayList<Integer>();
-        if(root==null) return res;
-        helper(res,root);
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) return res;
+        helper(res, root);
         return res;
     }
 
-    public void helper(List<Integer> res, TreeNode root){
-        if(root == null) return;
-        helper(res,root.left);
+    public void helper(List<Integer> res, TreeNode root) {
+        if (root == null) return;
+        helper(res, root.left);
         res.add(root.val);
-        helper(res,root.right);
+        helper(res, root.right);
     }
 
     public List<Integer> inorderTraversalStack(TreeNode root) {
-        List<Integer> res=new ArrayList<Integer>();
-        Stack<TreeNode> stack=new Stack<>();
-        if(root==null) return res;
-        TreeNode cur= root;
-        while(!stack.isEmpty()||cur!=null){
-            while(cur!=null){
+        List<Integer> res = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root == null) return res;
+        TreeNode cur = root;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
                 stack.push(cur);
-                cur=cur.left;
+                cur = cur.left;
             }
-            cur=stack.pop();
+            cur = stack.pop();
             res.add(cur.val);
-            cur=cur.right;
+            cur = cur.right;
         }
         return res;
     }
+}
