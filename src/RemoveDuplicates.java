7bb5f510b6d2e90f24d7420a1bb3c1e8820f8e56
FreddieSun <freddiesun95@gmail.com>
2018-02-26 12:38:41 -0500

    2/26

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -12,4 +12,15 @@ public class RemoveDuplicates {
         }
         return res;
     }
+
+    public int removeDuplicates2(int[] nums) {
+        if (nums.length < 2) return nums.length;
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != nums[j]) {
+                nums[++j] = nums[i];
+            }
+        }
+        return j+1;
+    }
 }
