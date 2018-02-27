1e8aedf0d86941588235e303464ead0a4b18aa5e
FreddieSun <freddiesun95@gmail.com>
2018-02-26 20:39:50 -0500

    2/26

---
+++ src/GetSum_Bit_Manipulation.java
@@ -0,0 +1,11 @@
+public class GetSum_Bit_Manipulation {
+    public int getSum(int a, int b) {
+        if (a == 0) return b;
+        if (b == 0) return a;
+
+        int sum = a ^ b;
+        int carry = (a & b) << 1;
+
+        return getSum(sum,carry);
+    }
+}
