f5ac8c87e4a73de14ff13af37b17bae6f953123e
FreddieSun <freddiesun95@gmail.com>
2018-03-01 15:19:18 -0500

    3/1

--- src/MaxProfit.java
+++ src/MaxProfit.java
@@ -2,10 +2,11 @@ public class MaxProfit {
     public class Solution {
         public int maxProfit(int[] prices) {
             int total = 0;
-            for (int i=0; i< prices.length-1; i++) {
-                if (prices[i+1]>prices[i]) total += prices[i+1]-prices[i];
+            for (int i = 0; i < prices.length - 1; i++) {
+                if (prices[i + 1] > prices[i]) total += prices[i + 1] - prices[i];
             }
 
             return total;
         }
+    }
 }
