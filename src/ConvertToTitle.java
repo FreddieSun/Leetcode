411c302a5d6dad16d80354589510c92c4e4a6003
FreddieSun <freddiesun95@gmail.com>
2018-03-01 15:23:37 -0500

    2/28

---
+++ src/ConvertToTitle.java
@@ -0,0 +1,11 @@
+public class ConvertToTitle {
+    public String convertToTitle(int n) {
+        StringBuilder res = new StringBuilder();
+        while (n > 0) {
+            n--;
+            res.insert(0, (char) ('A' + n % 26));
+            n /= 26;
+        }
+        return res.toString();
+    }
+}
