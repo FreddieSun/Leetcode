6c5f6cbe25284d83c0e84e13dc1b3454c3e74267
FreddieSun <freddiesun95@gmail.com>
2018-03-03 16:19:46 -0500

    3/3

---
+++ src/HIndex.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class HIndex {
+    public int hIndex(int[] citations) {
+
+        int res = 0;
+        int N = citations.length;
+        // 需要满足两个条件： 1. h of his N citations has at least h citations each.
+        //                 2. The other N-h papers no more than h citations each.
+        Arrays.sort(citations);
+
+        while (res < N && citations[N - res - 1] > res)
+            res++;
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,6,8};
+        int res = hIndex(nums);
+        System.out.println(res);
+    }
+}
