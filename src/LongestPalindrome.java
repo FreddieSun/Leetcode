ecf2ee329af2e6483546d3d99c49ea845451160f
FreddieSun <freddiesun95@gmail.com>
2018-03-09 16:01:30 -0500

    3/9

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -18,4 +18,27 @@ public class LongestPalindrome {
         }
         return res;
     }
+
+
+    String res = "";
+    public String longestPalindrome1(String s) {
+        if (s.length() == 0 || s == null)
+            return s;
+        for (int i = 0; i < s.length(); i++) {
+            helper(s, i, i);
+            helper(s, i, i + 1);
+        }
+        return res;
+    }
+
+    public void helper(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        String cur = s.substring(left + 1,right);
+        if (cur.length() > res.length()) {
+            res = cur;
+        }
+    }
 }
